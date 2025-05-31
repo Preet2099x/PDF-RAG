290dcf8a8bb103759150343cccde6cb5704e235a
Preet2099x <preet2099x@gmail.com>
2025-05-31 23:24:24 +0530

    added langchain to load and extract data from docs

--- server/worker.js
+++ server/worker.js
@@ -1,4 +1,11 @@
 import { Worker } from 'bullmq';
+import { QdrantVectorStore } from "@langchain/qdrant";
+import { OpenAIEmbeddings } from "@langchain/openai";
+// import type { Document } from "@langchain/core/documents";
+import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf";
+import { CharacterTextSplitter } from "@langchain/textsplitters";
+
+
 
 const worker = new Worker(
     'file-upload-queue',
@@ -12,6 +19,20 @@ const worker = new Worker(
         call the openai embedding model for every chunk,
         store the chunk in qdrant db
         */
+
+        //Loads the PDF
+        const loader = new PDFLoader(data.path);
+        const docs = await loader.load();
+
+        console.log(docs)
+
+        //Splits the PDF into chunks
+        // const textSplitter = new CharacterTextSplitter({
+        // chunkSize: 300,
+        // chunkOverlap: 0,
+        // });
+        // const texts = await textSplitter.splitText(docs);      
+        // console.log(texts)  
     },
     {
         concurrdency: 100, 
